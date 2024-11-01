4a0b862614123bb70a6904c2f7758955d90ee5f0
NahidKakon <nahidshakib@outlook.com>
2024-11-01 07:06:08 +0000

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,40 +1,99 @@
-import Link from 'next/link'
+import { useState } from 'react';
+import { navLinks } from '../utils/dataBase';
+import Image from 'next/image';
+import Button from './Button';
+import { MenuIcon, Rotate3D, X } from 'lucide-react';
+import DownloadButton from './DownloadButton';
 
-const navItems = {
-  '/': {
-    name: 'home',
-  },
-  '/blog': {
-    name: 'blog',
-  },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
-  },
-}
 
-export function Navbar() {
+
+function Nav({ className }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLinkClick = id => {
+    document
+      .getElementById(id)
+      .scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+    setIsOpen(false);
+  };
+
+  const renderLinks = () => {
+    return navLinks.map(({ id, label }) => (
+      <li
+        key={id}
+        className="text-1.4rem font-semibold text-gray-900 transition cursor-pointer hover:text-orange-500"
+        onClick={() => handleLinkClick(id)}
+      >
+        {label}
+      </li>
+    ));
+  };
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="lg:sticky lg:top-20">
-        <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
-          id="nav"
+    <nav
+      className={`z-50 p-4 transition duration-500 ease-out bg-white border-b ${className}`}
+    >
+      <div className="flex items-center justify-between max-w-6xl mx-auto">
+        <div className="z-30">
+          
+          <Image
+          
+            src="/logo.svg"
+            alt="logo"
+            className="transition-transform duration-300 cursor-pointer hover:-rotate-6"
+            width={100}
+            height={100}
+          
+          />
+
+
+        </div>
+
+        
+
+        <div className="items-center hidden gap-6 md:flex ">
+          <ul className="flex items-center gap-6 ">
+            {renderLinks()}
+          </ul>
+          <DownloadButton />
+          
+
+
+          {/* <Button className="text-black-700 bg-orange-400 hover:bg-orange-600">
+            Download CV
+            
+          </Button> */}
+        </div>
+
+        <button
+          className="z-30 md:hidden "
+          onClick={handleMenuToggle}
         >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
-                >
-                  {name}
-                </Link>
-              )
-            })}
-          </div>
-        </nav>
+          {isOpen ? <X /> : <MenuIcon />}
+        </button>
+
+        <div
+          className={`fixed bg-white md:hidden z-10 left-0 w-full p-4 transition-all duration-500 ease-in-out ${
+            isOpen
+              ? 'top-[4rem] translate-y-0 opacity-100'
+              : '-translate-y-[200%] opacity-0'
+          }`}
+        >
+          <ul className="flex flex-col items-center gap-4">
+            {renderLinks()}
+
+            {/* <Button className="text-white bg-orange-400 hover:bg-orange-600">
+              Download CV
+            
+            </Button> */}
+          </ul>
+        </div>
       </div>
-    </aside>
-  )
+    </nav>
+  );
 }
+
+export default Nav;
