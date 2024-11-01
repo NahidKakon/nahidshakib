77d2c1614f61b7e68f054463b713ae278f41650a
NahidKakon <nahidshakib@outlook.com>
2024-11-01 07:26:30 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,29 @@
-import { BlogPosts } from 'app/components/posts'
+import AboutSection from './sections/AboutSection';
+import ContactSection from './sections/ContactSection';
+import HeroSection from './sections/HeroSection';
+import ProjectsSection from './sections/ProjectsSection';
+import SevicesSection from './sections/SevicesSection';
+import TestimonialsSection from './sections/TestimonialsSection';
+import BackgroundBeamsWithCollisionDemo from '@/components/example/background-beams-with-collision-demo';
+import ThreeDCardDemo from '@/components/example/3d-card-demo';
+import DownloadButton from './components/DownloadButton';
+import Name from './components/Name';
+import Footer from './components/Footer';
 
-export default function Page() {
+export default function Home() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
-    </section>
-  )
+    <main className="w-full p-4">
+      
+      <HeroSection />
+      <AboutSection />
+      <SevicesSection />
+      <ProjectsSection />
+      {/* <TestimonialsSection /> */}
+      <ContactSection />
+      <Footer/>
+      {/* <BackgroundBeamsWithCollision/> */}
+      {/* <ThreeDCardDemo/> */}
+    
+    </main>
+  );
 }
