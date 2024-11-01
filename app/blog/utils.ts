db44fb7a2a6d51869389a560a610929620373589
NahidKakon <nahidshakib@outlook.com>
2024-11-01 07:40:10 +0000

    Update utils.ts

--- app/blog/utils.ts
+++ app/blog/utils.ts
@@ -1,90 +1,130 @@
-import fs from 'fs'
-import path from 'path'
 
-type Metadata = {
-  title: string
-  publishedAt: string
-  summary: string
-  image?: string
-}
 
-function parseFrontmatter(fileContent: string) {
-  let frontmatterRegex = /---\s*([\s\S]*?)\s*---/
-  let match = frontmatterRegex.exec(fileContent)
-  let frontMatterBlock = match![1]
-  let content = fileContent.replace(frontmatterRegex, '').trim()
-  let frontMatterLines = frontMatterBlock.trim().split('\n')
-  let metadata: Partial<Metadata> = {}
+import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
 
-  frontMatterLines.forEach((line) => {
-    let [key, ...valueArr] = line.split(': ')
-    let value = valueArr.join(': ').trim()
-    value = value.replace(/^['"](.*)['"]$/, '$1') // Remove quotes
-    metadata[key.trim() as keyof Metadata] = value
-  })
+export const navLinks = [
+   {id: 'name.svg' }, 
+  { id: 'home', label: 'Home' },
+  { id: 'about-Me', label: 'About Me' },
+  { id: 'services', label: 'Services' },
+  { id: 'projects', label: 'Projects' },
+//   { id: 'testimonials', label: 'Testimonials' },
+  { id: 'contact', label: 'Contact' },
+];
 
-  return { metadata: metadata as Metadata, content }
-}
+export const socialLinks = [
+  {
+    href: 'https://www.linkedin.com',
+    icon: <FaLinkedin className="size-5" />,
+    label: 'LinkedIn',
+  },
+  {
+    href: 'https://www.facebook.com',
+    icon: <FaFacebook className="size-5" />,
+    label: 'Facebook',
+  },
+  {
+    href: 'https://www.github.com',
+    icon: <FaGithub className="size-5" />,
+    label: 'GitHub',
+  },
+];
 
-function getMDXFiles(dir) {
-  return fs.readdirSync(dir).filter((file) => path.extname(file) === '.mdx')
-}
+export const skills = [
+  { name: 'Web development', value: 90 },
+  { name: 'Front-end design', value: 85 },
 
-function readMDXFile(filePath) {
-  let rawContent = fs.readFileSync(filePath, 'utf-8')
-  return parseFrontmatter(rawContent)
-}
+  { name: 'Back-end design', value: 88 },
+  { name: 'Special interest in sustainability (eg. social value, ESG) ', value: 70 },
+  { name: 'AI focused project ', value: 70 },
+];
 
-function getMDXData(dir) {
-  let mdxFiles = getMDXFiles(dir)
-  return mdxFiles.map((file) => {
-    let { metadata, content } = readMDXFile(path.join(dir, file))
-    let slug = path.basename(file, path.extname(file))
+export const projects = [
+  {
+    imgSrc: '/image1.gif',
+    altText: 'E-commerce Website',
+    title: 'E-commerce Website',
+  },
+  {
+    imgSrc: '/key_skills.gif',
+    altText: 'Portfolio Website',
+    title: 'Portfolio Website',
+  },
+  {
+    imgSrc: '/web/web-img-3.png',
+    altText: 'Mobile App Design',
+    title: 'Mobile App Design',
+  },
+  {
+    imgSrc: '/web/web-img-4.png',
+    altText: 'Corporate Website',
+    title: 'Corporate Website',
+  },
+  {
+    imgSrc: '/web/web-img-5.png',
+    altText: 'Landing Page Design',
+    title: 'Landing Page Design',
+  },
+  {
+    imgSrc: '/web/web-img-6.png',
+    altText: 'Social Media Dashboard',
+    title: 'Social Media Dashboard',
+  },
+];
 
-    return {
-      metadata,
-      slug,
-      content,
-    }
-  })
-}
+export const services = [
+  {
+    imgSrc: '/ui.svg',
+    altText: 'UI/UX Design',
+    title: 'UI/UX Design',
+    description:
+      'Creating intuitive and beautiful user interfaces that enhance user experiences and streamline interactions.',
+  },
+  {
+    imgSrc: '/web.svg',
+    altText: 'Web Design',
+    title: 'Web Design',
+    description:
+      'Designing responsive and visually stunning websites that provide seamless navigation across all devices.',
+  },
+  {
+    imgSrc: '/front.svg',
+    altText: 'App Design',
+    title: 'Front-end Development',
+    description:
+      'Developing functional and user friendly front-end for e-commerce, blogging, process flowchart and manymore.'},
+  {
+    imgSrc: '/back.svg',
+    altText: 'Graphic Design',
+    title: 'Back-end Development',
+    description:
+      'Building database, sign in functionality for secure use in e-commerce and others.',
+  },
+];
 
-export function getBlogPosts() {
-  return getMDXData(path.join(process.cwd(), 'app', 'blog', 'posts'))
-}
-
-export function formatDate(date: string, includeRelative = false) {
-  let currentDate = new Date()
-  if (!date.includes('T')) {
-    date = `${date}T00:00:00`
-  }
-  let targetDate = new Date(date)
-
-  let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear()
-  let monthsAgo = currentDate.getMonth() - targetDate.getMonth()
-  let daysAgo = currentDate.getDate() - targetDate.getDate()
-
-  let formattedDate = ''
-
-  if (yearsAgo > 0) {
-    formattedDate = `${yearsAgo}y ago`
-  } else if (monthsAgo > 0) {
-    formattedDate = `${monthsAgo}mo ago`
-  } else if (daysAgo > 0) {
-    formattedDate = `${daysAgo}d ago`
-  } else {
-    formattedDate = 'Today'
-  }
-
-  let fullDate = targetDate.toLocaleString('en-us', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-  })
-
-  if (!includeRelative) {
-    return fullDate
-  }
-
-  return `${fullDate} (${formattedDate})`
-}
+export const testimonials = [
+  {
+    name: 'John Doe',
+    review:
+      'Working with you was an absolute pleasure. Your attention to detail and creativity made the process smooth and enjoyable. I am thrilled with the final product!',
+    imgSrc: '/profile/profile-1.jpg ',
+  },
+//   {
+//     name: 'Jane Smith',
+//     review:
+//       'The design exceeded my expectations! You understood my needs perfectly and delivered a stunning website that truly reflects my brand.',
+//     imgSrc: '/profile/profile-2.jpg',
+//   },
+//   {
+//     name: 'Michael Lee',
+//     review:
+//       'Your professionalism and dedication to the project were outstanding. I appreciate how you went above and beyond to make sure everything was perfect!',
+//     imgSrc: '/profile/profile-3.jpg',
+//   },
+//   {
+//     name: 'Emily Davis',
+//     review:
+//       'I was blown away by your ability to bring my vision to life. The end result is beautiful and exactly what I was hoping for. Thank you!',
+//     imgSrc: '/profile/profile-4.jpg',
+//   },
+];
